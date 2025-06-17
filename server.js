b2fb80dacb0f7a864b958d596b3a1f5209152e4a
dianallar <marcusass2002@gmail.com>
2025-06-17 09:14:07 -0400

    fixed cors error

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Add CORS configuration before other middleware
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://dianallar.github.io',
     credentials: true
 }));
 
